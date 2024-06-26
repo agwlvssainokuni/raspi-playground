4a26bb278b76cd56e965a49380c7328a35f23cdd
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2024-06-26 21:50:56 +0900

    最小限のSpring Boot構成。

---
+++ boardinfo/src/main/java/cherry/raspi/Main.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2024 agwlvssainokuni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cherry.raspi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        System.exit(doMain(args));
+    }
+
+    public static int doMain(String[] args) {
+        try (var appctx = SpringApplication.run(Main.class, args)) {
+            return SpringApplication.exit(appctx);
+        }
+    }
+
+}
